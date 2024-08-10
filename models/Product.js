01dd1c5066c8fe2404f849a915ae0357e3c64a34
unknown <travihay@gmail.com>
2024-08-10 14:28:21 -0500

    added js to models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      allowNull: false,
+      primaryKey: false,
+      autoIncrement: true,
+    },
+    price: {
+      type:DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+        isDecimal:true,
+      }
+    },
+    stock: {
+      type:DataTypes.INTEGER,
+      allowNuLL: false,
+      defaultValue:10,
+      validate: {
+        isNumeric:true,
+      },
+    },
+    category_id: {
+      type:DataTypes.INTEGER,
+      refrences: {
+        model: 'category',
+        key: 'id' ,
+      },
+    },
   },
   {
     sequelize,
